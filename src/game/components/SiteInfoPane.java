67ab40d3341470fe30d38d3f3cfce4750cbb1219
Eddie <eddie@ubuntu.(none)>
2014-03-13 03:19:41 +0000

    worked on site info pop-ups from map markers

--- src/game/components/SiteInfoPane.java
+++ src/game/components/SiteInfoPane.java
@@ -9,21 +9,37 @@ import game.swingFramework.FrontEndPane;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 import javax.swing.JWindow;
 import javax.swing.Timer;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+import java.util.List;
 
 public class SiteInfoPane extends JFrame {
-	
+	List<Site> sites;
+	JPanel modules;
 	public SiteInfoPane(String name) {
 		this.setTitle(name);
 		this.setMinimumSize(new Dimension(300, 300));
 		this.setPreferredSize(new Dimension(300, 300));
-		
-	
-		
+		sites = game.swingFramework.FrontEndPane.modSiteController.allSites;
+		modules = new JPanel();
+		modules.setLayout(new GridLayout(0,1));
+		for (Site s : sites) {
+			for (Module m : s.modules) {
+				if (name.split(" Site Info")[0].equals(s.siteName)) {
+					modules.add(new JLabel("Name: " + m.getName()));
+					modules.add(new JLabel("Work done: " + m.workDone() + " hours"));
+					modules.add(new JLabel("Sections completed: " + m.sectionsCompleted()));
+				}
+			}
+		}
+		this.add(modules);		
 		this.pack();
 		this.setVisible(true);
 	}
